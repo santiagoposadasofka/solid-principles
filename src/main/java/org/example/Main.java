b693b8fd7961d53c1e3082fb650803892d1619f7
juan david cardona velasquez <j.cardona1@utp.edu.co>
2023-01-26 10:11:12 -0500

    Todos los principios finalizados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.dependecyInversion.DependencyInvesion;
+import org.example.interfaceSegregation.InterfaceSegregation;
 import org.example.liskovSubstitution.LiskovSubstitution;
 import org.example.openClose.OpenClose;
 import org.example.singleResponsibility.Database;
@@ -7,23 +9,33 @@ import org.example.singleResponsibility.SingleResponsibility;
 
 public class Main {
     public static void main(String[] args) {
-//        LiskovSubstitution liskovSubstitution = new LiskovSubstitution();
-//        liskovSubstitution.ejecutar();
+        LiskovSubstitution liskovSubstitution = new LiskovSubstitution();
+        liskovSubstitution.ejecutar();
 
 
-//        // Instancia de Single Responsibility y ejecucion de Cuenta Bancaria
-//        SingleResponsibility single = new SingleResponsibility();
-//        single.ejecutar();
-//
-//        //Anti Patron Sistema
-//        single.ejecutarSistema();
-//        //Nueva instancia de Student que aplica Single Responsibility
-//        single.ejecutarStudent();
+        //Instancia de Single Responsibility y ejecucion de Cuenta Bancaria
+        SingleResponsibility single = new SingleResponsibility();
+        single.ejecutar();
 
-        // Instancia de Open Close
+        //Anti Patron Sistema
+        single.ejecutarSistema();
+        //Nueva instancia de Student que aplica Single Responsibility
+        single.ejecutarStudent();
+
+        /// Instancia de Open Close
         OpenClose open =  new OpenClose();
         open.ejecutarPrincipio();
         open.ejecutarAntiPatron();
         open.ejecutarnuevoAntiPatron();
+
+        //Instancia de Interface Segregation
+        InterfaceSegregation inter = new InterfaceSegregation();
+        inter.ejecutar();
+        inter.ejecutarEjemplo();
+
+        //Instancia de Dependency inversion
+        DependencyInvesion dependency = new DependencyInvesion();
+        dependency.ejecutar();
+        dependency.ejecutarEjemplo();
     }
 }
--- src/main/java/org/example/dependecyInversion/DependencyInvesion.java
+++ src/main/java/org/example/dependecyInversion/DependencyInvesion.java
@@ -4,6 +4,31 @@ public class DependencyInvesion {
     /**
      * Se Sigue con la dinamica de los principios previos
      * */
+    public void ejecutar(){
+        /**
+         * Se Sigue con la dinamica de los principios previos
+         * */
+        System.out.println("-------------Principio Dependency Inversion----------\n");
+        GmailService gmailService = new GmailService();
+        OutlookService outlookService = new OutlookService();
+
+        Empleado SantiagoPosada = new Empleado(gmailService);
+        Empleado JesusMiguel = new Empleado(outlookService);
+        SantiagoPosada.enviarCorreo("Julia", "Urgente", "Como sigues de Salud");
+        JesusMiguel.enviarCorreo("Andrea", "Invitacion", "Te invito a mi fiesta de aniversario");
+        }
+    public void ejecutarEjemplo(){
+        System.out.println("------Nuevo ejemplo de Dependency Inversion-------\n");
+        LlamadaNacional nacional = new LlamadaNacional();
+        LlamadaInternacional internacional = new LlamadaInternacional();
+
+        PersonaQueLlama Juan = new PersonaQueLlama(nacional);
+        PersonaQueLlama Daniela = new PersonaQueLlama(internacional);
+        Juan.llamar(3214587);
+        Daniela.llamar(3698541);
+    }
+
+
 }
 
 
@@ -14,7 +39,6 @@ public class DependencyInvesion {
 * se proporciona una instancia de "ServicioDeCorreo" a través de un constructor o un setter. De esta forma,
 * "Empleado" no tiene conocimiento de cómo se envían los correos electrónicos, sino que simplemente utiliza la interfaz.
 
-
  * */
 
 interface ServicioDeCorreo {
@@ -24,14 +48,14 @@ interface ServicioDeCorreo {
 class GmailService implements ServicioDeCorreo {
     @Override
     public void enviarCorreo(String destinatario, String asunto, String mensaje) {
-        // Envia correo utilizando la API de Gmail
+        System.out.println("Enviando correo de gmail a "+destinatario+" con asunto "+asunto+" diciendo "+mensaje);
     }
 }
 
 class OutlookService implements ServicioDeCorreo {
     @Override
     public void enviarCorreo(String destinatario, String asunto, String mensaje) {
-        // Envia correo utilizando la API de Outlook
+        System.out.println("Enviando correo de Outlook a "+destinatario+" con asunto "+asunto+" diciendo "+mensaje);
     }
 }
 
@@ -58,6 +82,42 @@ class Empleado {
     }
 }
 
+
+//Creacion del nuevo ejemplo para hacer llamadas
+
+interface ServiciodeLlamadas {
+    void hacerllamada(int numero);
+}
+
+
+class LlamadaNacional implements ServiciodeLlamadas {
+    @Override
+    public void hacerllamada(int numero) {
+        System.out.println("Haciendo una llamada Nacional a "+numero);
+    }
+}
+
+class LlamadaInternacional implements ServiciodeLlamadas {
+    @Override
+    public void hacerllamada(int numero) {
+        System.out.println("Haciendo una llamada internacional a "+numero);
+    }
+}
+
+class PersonaQueLlama {
+    private ServiciodeLlamadas serviciodeLlamadas;
+
+    public PersonaQueLlama(ServiciodeLlamadas serviciodeLlamadas) {
+        this.serviciodeLlamadas = serviciodeLlamadas;
+    }
+
+    public void setServiciodeLlamadas(ServiciodeLlamadas serviciodeLlamadas) {
+        this.serviciodeLlamadas = serviciodeLlamadas;
+    }
+    public void llamar(int numero) {
+        serviciodeLlamadas.hacerllamada(numero);
+    }
+}
 
 
 
