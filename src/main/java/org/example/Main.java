32d4eccc8ad81d41eaa4595256f9e7d9dd8b3c99
POO <juandieguin2016@gmail.com>
2023-01-25 22:35:27 -0500

    liskov Substitution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,21 @@ package org.example;
 
 import org.example.liskovSubstitution.LiskovSubstitution;
 
+import org.example.openClose.OpenClose;
 import org.example.singleResponsibility.SingleResponsibility;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("---SINGLE RESPONSABILITY---");
+
         SingleResponsibility single = new SingleResponsibility();
         single.ejecutar();
+
+        System.out.println("---LISKOV SUBSTITUTION---");
+
+        LiskovSubstitution liskov = new LiskovSubstitution();
+        liskov.ejecutar();
 
     }
 }
