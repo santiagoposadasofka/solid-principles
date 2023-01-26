fbdede9af1adb50d5f80e93f75b783157a13a151
nunezjose770 <nunezjose770@gmail.com>
2023-01-26 09:11:17 -0500

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,27 @@
 package org.example;
 
+import org.example.liskovSubstitution.EjemploAntipatron;
 import org.example.liskovSubstitution.LiskovSubstitution;
 import org.example.openClose.OpenClose;
 import org.example.singleResponsibility.SingleResponsibility;
 
 public class Main {
     public static void main(String[] args) {
-       /**
-        LiskovSubstitution liskovSubstitution = new LiskovSubstitution();
-        liskovSubstitution.ejecutar();
-
+        System.out.println("------Patron SingleResponsability-----");
+        /**
         SingleResponsibility single = new SingleResponsibility();
         single.ejecutar();
         **/
-
+        System.out.println("----------Patron Openclose-----------");
         OpenClose openClose = new OpenClose();
         openClose.ejecutar();
+        System.out.println("-----------Patron liskov-------------");
+        LiskovSubstitution liskovSubstitution = new LiskovSubstitution();
+        liskovSubstitution.ejecutar();
+        System.out.println("----------Antipatron liskov----------");
+        EjemploAntipatron ejemploAntipatron = new EjemploAntipatron();
+        ejemploAntipatron.ejecutar();
+        System.out.println("-----------Patron interfaz-----------");
+
     }
 }
--- src/main/java/org/example/interfaceSegregation/InterfaceSegregation.java
+++ src/main/java/org/example/interfaceSegregation/InterfaceSegregation.java
@@ -12,7 +12,7 @@ public class InterfaceSegregation {
 * para volar y aterrizar. Sin embargo, no todos los objetos que implementan la interfaz "Volador"
 * tienen la capacidad de navegar. Por lo tanto, se crea una interfaz separada "Navegable" que solo tiene métodos para navegar.
 * En este ejemplo se puede ver cómo se han separado las interfaces Volador y Navegable, de esta forma solo se tiene que
-* implementar los métodos ne
+* implementar los métodos nesarios
 * */
 
 interface Volador {
