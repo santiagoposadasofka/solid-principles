059caf10a508e150e411d4af0fe75de3bdcd2909
Ivan <idruizbe@gmail.com>
2023-01-26 01:03:35 -0500

    primer ejercicio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import org.example.liskovSubstitution.LiskovSubstitution;
+import org.example.singleResponsibility.SingleResponsibility;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        var liskovSubstitution = new LiskovSubstitution();
+        SingleResponsibility.ejecutar();
+        System.out.println("\n");
+        LiskovSubstitution liskovSubstitution = new LiskovSubstitution();
         liskovSubstitution.ejecutar();
     }
 }
--- src/main/java/org/example/singleResponsibility/SingleResponsibility.java
+++ src/main/java/org/example/singleResponsibility/SingleResponsibility.java
@@ -1,4 +1,7 @@
 package org.example.singleResponsibility;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SingleResponsibility {
     /**
@@ -7,15 +10,47 @@ public class SingleResponsibility {
      * van a ejecutar sus comportamientos.
      * Van a establecer un ejemplo de una clase que sigue este patron o una clase que no lo sigue.
      * */
+    public static void ejecutar(){
+        ImprimirRecibo imprimirRecibo=new ImprimirRecibo();
+        Printer imp=new Printer();
+        Database db=new Database();
+        CuentaBancaria cuenta=new CuentaBancaria(2100);
+        cuenta.depositar(4400);
+        imp.print("Se hizo un correcto depósito de 4400, su nuevo saldo es: "+cuenta.getSaldo());
+        db.saveTransaction("Deposit. 00001");
+        cuenta.retirar(3500);
+        imp.print("Se realizó un retiro de 3500, su nuevo saldo es: "+cuenta.getSaldo());
+        db.saveTransaction("Ret. 00002");
+        System.out.println("\n");
+        Sistema sistema=new Sistema();
+        sistema.depositar(3500);
+        sistema.retirar(1000);
+        System.out.println("\n");
+        imprimirRecibo.imprimirRecibo(db,"retiro");
+    }
+}
+class ImprimirRecibo{
+    Printer imp=new Printer();
+    Fecha date=new Fecha();
+    public void imprimirRecibo(Database database,String motivo){
+        imp.print("Se ha creado el recibo No. "+ ThreadLocalRandom.current().nextInt(100,1001)
+                +"\n\nCon fecha "+date.obtenerFecha(LocalDateTime.now()) +
+                "\nrealizando un "+motivo+" exitosamente");
+    }
+}
+class Fecha{
+    public String obtenerFecha(LocalDateTime ahora){
+        return ahora.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
 }
 
-
 /*
- * Un ejemplo de patrón de responsabilidad única en Java podría ser una clase "CuentaBancaria" que solo se encarga de las operaciones
- * relacionadas con una cuenta bancaria, como depositar o retirar dinero. Esta clase solo tendría responsabilidades relacionadas con la cuenta
- * bancaria y no se preocuparía por otras tareas, como el envío de correos electrónicos o la impresión de recibos.
- * La clase tiene un solo método de negocio, depositar y retirar dinero, y un método de consulta, getSaldo, esto se asegura que la clase tenga
- * una sola responsabilidad, en este caso, administrar las transacciones de la cuenta bancaria y no hacer otra cosa.
+ * Un ejemplo de patrón de responsabilidad única en Java podría ser una clase "CuentaBancaria" que solo se encarga de
+ * las operaciones relacionadas con una cuenta bancaria, como depositar o retirar dinero. Esta clase solo tendría
+ * responsabilidades relacionadas con la cuenta bancaria y no se preocuparía por otras tareas, como el envío de correos
+ * electrónicos o la impresión de recibos. La clase tiene un solo método de negocio, depositar y retirar dinero, y un
+ * método de consulta, getSaldo, esto se asegura que la clase tenga una sola responsabilidad, en este caso, administrar
+ * las transacciones de la cuenta bancaria y no hacer otra cosa.
  * */
 
 class CuentaBancaria {
@@ -42,12 +77,13 @@ class CuentaBancaria {
 }
 
 /*
- * Un anti-patrón del principio de responsabilidad única en Java podría ser una clase "Sistema" que hace muchas cosas diferentes,
- * como manejar transacciones bancarias, enviar correos electrónicos, imprimir recibos, realizar cálculos y almacenar información en
- * una base de datos. Esta clase tendría muchas responsabilidades diferentes, lo que dificultaría su comprensión, su mantenimiento y su escalabilidad.
- * En este ejemplo se ve como la clase Sistema tiene varias responsabilidades, manejar transacciones bancarias, enviar correos, imprimir recibos,
- * almacenar información en una base de datos, esto dificulta su comprensión, su mantenimiento y escalabilidad. Es mejor crear varias clases diferentes
- * para cada responsabilidad.
+ * Un anti-patrón del principio de responsabilidad única en Java podría ser una clase "Sistema" que hace muchas cosas
+ * diferentes, como manejar transacciones bancarias, enviar correos electrónicos, imprimir recibos, realizar cálculos y
+ * almacenar información en una base de datos. Esta clase tendría muchas responsabilidades diferentes, lo que
+ * dificultaría su comprensión, su mantenimiento y su escalabilidad. En este ejemplo se ve como la clase Sistema tiene
+ * varias responsabilidades, manejar transacciones bancarias, enviar correos, imprimir recibos, almacenar información en
+ * una base de datos, esto dificulta su comprensión, su mantenimiento y escalabilidad. Es mejor crear varias clases
+ * diferentes para cada responsabilidad.
  * */
 
 class Sistema {
