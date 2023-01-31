f7f434f4121d31674acd785b4dfb59df9eef74e4
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2023-01-30 20:04:18 -0600

    Principio de Liskov Substitution ejemplo Open Bootcamp

---
+++ src/main/java/org/example/LisKovSubstitutionOpenBootCamp/Usuarios.java
@@ -0,0 +1,41 @@
+package org.example.LisKovSubstitutionOpenBootCamp;
+
+
+
+import java.util.ArrayList;
+
+public class Usuarios {
+    UsuariosDB usuariosDB;
+
+    public Usuarios(UsuariosDB usuariosDB) {
+        this.usuariosDB = usuariosDB;
+    }
+
+    protected Usuarios() {}
+
+    public ArrayList<Usuario> listarUsuarios() {
+        return usuariosDB.obtener();
+    }
+
+    public Usuario obtenerUsuario(String username) {
+        Usuario usuario = new Usuario();
+        usuario.nombreUsuario = username;
+
+        return usuariosDB.buscar(usuario);
+    }
+
+    public void crearUsuario(Usuario usuario) {
+        if (usuariosDB.buscar(usuario) != null) {
+            return;
+        }
+
+        usuariosDB.insertar(usuario);
+    }
+
+    public void borrarUsuario(String username) {
+        Usuario usuario = new Usuario();
+        usuario.nombreUsuario = username;
+
+        usuariosDB.borrar(usuario);
+    }
+}
